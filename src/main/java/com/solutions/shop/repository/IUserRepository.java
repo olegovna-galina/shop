84ff487b923bd197b02114732abc45d497eba990
olegovna-galina <galga_1@tut.by>
2022-09-27 13:50:11 +0300

    adding all tables in DB;

--- src/main/java/com/solutions/shop/repository/IUserRepository.java
+++ src/main/java/com/solutions/shop/repository/IUserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IUserRepository extends JpaRepository<Users, String> {
+public interface IUserRepository extends JpaRepository<Users, Integer> {
 }
